4b75eccc32703356511f1c992479a2df4b46113f
smiyaguchi <smiyaguchi96@gmail.com>
2018-02-15 14:44:21 +0000

    Fix run command arg

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 #[macro_use]
 extern crate clap;
+extern crate nix;
 
 use clap::App;
+use nix::unistd::{chdir, getcwd};
 
 fn main() {
     let yaml = load_yaml!("../cli.yml");
-    let _matches = App::from_yaml(yaml).get_matches();
+    let matches = App::from_yaml(yaml).get_matches();
+
+    if let Some(ref matches) = matches.subcommand_matches("run") {
+        println!("cwd is {:?}", getcwd().unwrap());
+        let rootfs = matches.value_of("rootfs").unwrap();
+        chdir(rootfs).unwrap();
+        println!("cwd is {:?}", getcwd().unwrap());
+    }
 }
